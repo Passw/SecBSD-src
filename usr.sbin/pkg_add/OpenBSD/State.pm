7a404394cfdfaa0f43b84de7e1cefda15850948f
purplerain <purplerain@secbsd.org>
2023-11-25 18:12:55 +0000

    sync with OpenBSD -current

--- usr.sbin/pkg_add/OpenBSD/State.pm
+++ usr.sbin/pkg_add/OpenBSD/State.pm
@@ -1,5 +1,5 @@
 # ex:ts=8 sw=4:
-# $OpenBSD: State.pm,v 1.75 2023/11/24 18:19:25 espie Exp $
+# $OpenBSD: State.pm,v 1.77 2023/11/25 10:18:40 espie Exp $
 #
 # Copyright (c) 2007-2014 Marc Espie <espie@openbsd.org>
 #
@@ -251,6 +251,7 @@ sub handle_options($state, $opt_string, @usage)
 	});
 	$state->{v} = $state->opt('v');
 
+	# XXX don't try to move to AddCreateDelete, PkgInfo needs this too
 	if ($state->defines('unsigned')) {
 		$state->{signature_style} //= 'unsigned';
 	} elsif ($state->defines('oldsign')) {
@@ -311,18 +312,5 @@ sub find_window_size($self)
 		};
 	}
 }
-
-OpenBSD::Auto::cache(signer_list,
-	sub($self) {
-		if ($self->defines('SIGNER')) {
-			return [split /,/, $self->{subst}->value('SIGNER')];
-		} else {
-			if ($self->defines('FW_UPDATE')) {
-				return [qr{^.*fw$}];
-			} else {
-				return [qr{^.*pkg$}];
-			}
-		}
-	});
 
 1;
