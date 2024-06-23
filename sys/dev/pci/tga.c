7bc640af071ff8ebcaca71ad7767fdc81f50a2a2
purplerain <purplerain@secbsd.org>
2024-06-23 03:24:22 +0000

    sync with OpenBSD -current

--- sys/dev/pci/tga.c
+++ sys/dev/pci/tga.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tga.c,v 1.43 2022/07/15 17:57:26 kettenis Exp $ */
+/* $OpenBSD: tga.c,v 1.44 2024/06/22 10:22:29 jsg Exp $ */
 /* $NetBSD: tga.c,v 1.40 2002/03/13 15:05:18 ad Exp $ */
 
 /*
@@ -548,8 +548,8 @@ tgaattach(parent, self, aux)
 #endif
 }
 
-void
-tga_config_interrupts (d)
+void 
+tga_config_interrupts(d)
 	struct device *d;
 {
 	struct tga_softc *sc = (struct tga_softc *)d;
@@ -1435,7 +1435,7 @@ tga_eraserows(c, row, num, attr)
 }
 
 int
-tga_erasecols (c, row, col, num, attr)
+tga_erasecols(c, row, col, num, attr)
 	void *c;
 	int row, col, num;
 	uint32_t attr;
